687f969c59324a40c365bf4c210dc6482f1409b0
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-02-01 16:24:30 +0500

    add details

--- src/home.js
+++ src/home.js
@@ -1,7 +1,20 @@
 export default  function loadHome() {
     const content = document.getElementById('content');
     content.innerHTML = ''
+
     const heading = document.createElement('h1')
     heading.textContent = 'Restaurant X'
     content.appendChild(heading)
+
+    const about = document.createElement('div')
+    about.id = 'about'
+    content.appendChild(about)
+
+    const aboutH3 = document.createElement('h3')
+    aboutH3.textContent = 'About'
+    about.appendChild(aboutH3)
+
+    const para = document.createElement('p')
+    para.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate laudantium velit? Laboriosam optio adipisci, reprehenderit fugit eveniet velit voluptatum animi voluptates inventore hic fugiat illo quis unde saepe quod'
+    about.appendChild(para)
 }
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,21 @@
 export default  function loadMenu() {
     const content = document.getElementById('content');
     content.innerHTML = ''
+    
     const heading = document.createElement('h1')
     heading.textContent = 'Menu X'
+
     content.appendChild(heading)
+    const about = document.createElement('div')
+    about.id = 'about'
+    content.appendChild(about)
+
+    const aboutH3 = document.createElement('h3')
+    aboutH3.textContent = 'About'
+    about.appendChild(aboutH3)
+
+    const para = document.createElement('p')
+    para.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate laudantium velit? Laboriosam optio adipisci, reprehenderit fugit eveniet velit voluptatum animi voluptates inventore hic fugiat illo quis unde saepe quod'
+    about.appendChild(para)
+
 }
